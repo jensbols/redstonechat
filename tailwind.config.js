df1e489831313ff4460be8adab2ecca2a4263d97
Jens Bols <jens.bols@student.hogent.be>
2022-03-12 11:02:33 +0100

    added configurations

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'redstone': '#720000',
+      'blackstone': '#000000',
+      'greenstone': '#077728',
+      'whitestone': '#FFFFFF'
+    }
+  },
+  plugins: [],
+}
