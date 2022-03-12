8bdbbe0dcfe3bc26ff16baa6abac52a38eed14d4
Jens Bols <jens.bols@student.hogent.be>
2022-03-12 11:02:22 +0100

    added video section partly

---
+++ src/components/VideoSection.jsx
@@ -0,0 +1,10 @@
+export default function VideoSection() {
+  return (
+    <>
+    <h2 className="text-whitestone text-2xl font-bold text-center mt-6">How to join a Server</h2>
+    <div className="border-whitestone border-2 h-40 my-3 mx-3">
+      <p className="text-whitestone flex justify-center">Youtube Video</p>
+    </div>
+    </>
+  )
+}
